eecbd4a847868621f3bbd054d587b4a606849d06
michael-junior <michaeljunioroliveira@gmail.com>
2022-11-04 00:19:28 -0300

    Extração do metodo export_files e implementação de exceção na conversão do xml

--- python/e2m/xml2mongo.py
+++ python/e2m/xml2mongo.py
@@ -1,7 +1,6 @@
 import argparse
 import datetime
 import os
-import logging
 
 import xmltodict
 import xml.etree.ElementTree as ET
@@ -14,6 +13,7 @@ from mongodb.mongoExport import MongoExport
 
 @dataclass
 class X2M_Parameters:
+    tagMain: str
     xmlDir: str
     database: str
     collection: str
@@ -33,35 +33,50 @@ class Xml2Mongo:
 
     def export_files(self: X2M_Parameters):
 
-        export_mongo: MongoExport = MongoExport(self.database, self.collection, self.clear,
-                                                self.host, self.port, self.user, self.password)
         os.chdir(self.xmlDir)
         path_xml = os.listdir()
+
         xml_encod: str = self.xmlFileEncod or "utf-8"
+        export_mongo: MongoExport = MongoExport(self.database, self.collection, self.clear,
+                                                self.host, self.port, self.user, self.password)
 
-        print(f"\n{datetime.datetime.now().strftime('Export start time %H:%M')}\nTotal files for export:"
-              f" {len(path_xml)}\n")
+        print(f"\n--{datetime.datetime.now().strftime('Export start time %H:%M')}--")
+        print(f"Total files for export:"f" {len(path_xml)}\n")
 
-        for xml in (xml for xml in path_xml if xml.endswith(".xml")):
+        for xml in (xml for xml in path_xml if Xml2Mongo.str2elem(xml)):
             try:
-                with open(xml, 'r') as xml_file:
-                    tree = ET.parse(xml_file)
-                    root = tree.getroot()
-                    print(f"Name File: {xml}")
-                    for row in tqdm(root.findall('PubmedArticle')):
-                        xml_str = ET.tostring(row, encoding=xml_encod)
-                        doc = xmltodict.parse(xml_str, attr_prefix='', cdata_key='text')
-                        export_mongo.insert(doc)
-                xml_file.close()
+                print(f"Name File: {xml}")
+                for row in tqdm(Xml2Mongo.str2elem(xml).findall(self.tagMain)):
+                    xml_str = ET.tostring(row, encoding=xml_encod)
+                    doc = xmltodict.parse(xml_str, attr_prefix='', cdata_key='text')
+                    export_mongo.insert(doc)
             except Exception as e:
                 print(e)
+
+        print(f"\n--{datetime.datetime.now().strftime('Export end time %H:%M')}--\n")
+
+    @staticmethod
+    def str2elem(xml):
+        try:
+            with open(xml, 'r') as xml_file:
+                tree = ET.parse(xml_file)
+                xml_re = tree.getroot()
+                xml_file.close()
+                return xml_re
+
+        except Exception:
+            print(f"Name File: {xml}\nFILE CONVERSION ERROR")
 
 
 class main:
     parse = argparse.ArgumentParser(description='Argumentos requeridos: -xmlDir, -database, -collection.')
 
+    parse.add_argument('--tagMain', action='store', dest='tagMain',
+                       required=True, help='Retorna a lista contendo todos os elementos correspondentes na ordem do '
+                                           'documento.'),
+
     parse.add_argument('-xmlDir', action='store', dest='xmlDir',
-                       required=True, help='diretório dos arquivos XML'),
+                       required=True, help='Diretório dos arquivos XML'),
 
     parse.add_argument('-database', action='store', dest='database',
                        required=True, help='Nome do banco de dados MongoDB')
@@ -83,30 +98,30 @@ class main:
 
     parse.add_argument('-xmlFilter', action='store', dest='xmlFilter',
                        required=False,
-                       help='se presente, usa a expressão regular para filtrar os nomes de arquivo xml desejados')
+                       help='Se presente, usa a expressão regular para filtrar os nomes de arquivo xml desejados')
 
     parse.add_argument('-xmlFileEncod', action='store', dest='xmlFileEncod',
-                       required=False, help='se presente, indique a codificação do arquivo xml. O padrão é utf-8')
+                       required=False, help='Se presente, indique a codificação do arquivo xml. O padrão é utf-8')
 
     parse.add_argument('-logFile', action='store', dest='logFile',
                        required=False,
-                       help='se presente, indique o nome de um arquivo de log com os nomes dos arquivos '
+                       help='Se presente, indique o nome de um arquivo de log com os nomes dos arquivos '
                             'XML que não foram importados por causa de bugs')
 
     parse.add_argument('--recursive', action='store_true', dest='recursive',
-                       required=False, help='se presente, procure por documentos xml em subdiretórios')
+                       required=False, help='Se presente, procure por documentos xml em subdiretórios')
 
     parse.add_argument('--clear', action='store_true', dest='clear',
                        required=False,
-                       help='se presente, limpe todos os documentos da coleção antes de importar novos')
+                       help='Se presente, limpe todos os documentos da coleção antes de importar novos')
 
     parse.add_argument('--bulkWrite', action='store_true', dest='bulkWrite',
-                       required=False, help='se estiver presente irá escrever muitos documentos no MongoDb a cada '
+                       required=False, help='Se estiver presente irá escrever muitos documentos no MongoDb a cada '
                                             'iteração (requer mais RAM disponível')
 
     args = parse.parse_args()
 
-    params: X2M_Parameters = X2M_Parameters(args.xmlDir, args.database, args.collection,
+    params: X2M_Parameters = X2M_Parameters(args.tagMain, args.xmlDir, args.database, args.collection,
                                             args.host, args.port, args.user, args.password,
                                             args.xmlFilter, args.xmlFileEncod,
                                             args.logFile, args.recursive, args.clear,
